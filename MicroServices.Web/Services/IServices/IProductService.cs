449b2b0c0c20cdb32dce99b67840d3a94b0a07cf
Felipe Luiz <fel13.luiz@gmail.com>
2022-08-15 07:32:15 -0300

    create front web solution

---
+++ MicroServices.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using MicroServices.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MicroServices.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
