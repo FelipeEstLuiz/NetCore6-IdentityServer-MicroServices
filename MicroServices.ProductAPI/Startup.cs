15a96a661ad1016275a9d5e4f7226d68af145708
Felipe Luiz <fel13.luiz@gmail.com>
2022-08-17 07:48:20 -0300

    create product index

--- MicroServices.ProductAPI/Startup.cs
+++ MicroServices.ProductAPI/Startup.cs
@@ -25,7 +25,7 @@ namespace MicroServices.ProductAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
 
             services.AddDbContext<SqlServerContext>(
                 options => options.UseSqlServer(
---
+++ MicroServices.Web/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using MicroServices.Web.Models;
+using MicroServices.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServices.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            IEnumerable<ProductModel> products = await _productService.FindAllProducts();
+
+            return View(products);
+        }
+    }
+}
