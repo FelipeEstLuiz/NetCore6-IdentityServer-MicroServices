096bb573287354280bef0bf38fa1801f7948f26d
Felipe Luiz <fel13.luiz@gmail.com>
2022-08-22 07:24:28 -0300

    criacao api identity server

---
+++ MicroServices.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace MicroServices.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResource => new List<IdentityResource>()
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>()
+        {
+            new ApiScope("microservice_shopping", "MicroServiceShopping Server"),
+            new ApiScope(name: "read", "Read data."),
+            new ApiScope(name: "write", "Write data."),
+            new ApiScope(name: "delete", "Delete data.")
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client>()
+        {
+            new Client()
+            {
+                ClientId = "client",
+                ClientSecrets = { new Secret("microservices_felipe_estevam".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "read", "write", "profile" }
+            }
+        };
+    }
+}
---
+++ MicroServices.IdentityServer/Startup.cs
@@ -0,0 +1,60 @@
+using MicroServices.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MicroServices.IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+
+            services.AddDbContext<SqlServerContext>(
+                options => options.UseSqlServer(
+                    connectionString,
+                    b => b.MigrationsAssembly(typeof(SqlServerContext).Assembly.FullName)
+                )
+            );
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
